6e52f208782323b225c96d0ce0f581069a5ffc69
victor-delfino <hugo.delfino11@gmail.com>
2024-02-14 19:02:43 -0300

    Primeiro Commit

---
+++ entities/Client.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Client {
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private String name;
+	private String email;
+	private Date birthDate;
+	
+	public Client(String name, String email, Date birthDate) {
+		super();
+		this.name = name;
+		this.email = email;
+		birthDate = birthDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		birthDate = birthDate;
+	}
+
+	@Override
+	public String toString() {
+		return name + " (" + sdf.format(birthDate) + ") - " + email;
+	}
+	
+}
